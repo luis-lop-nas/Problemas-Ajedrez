4290049166d376e53d6dbfe7810a363e373f388c
luis-lop-nas <luislopnas@icloud.com>
2025-04-08 19:11:04 +0200

    cambios en tablero

--- reina/tablero.py
+++ reina/tablero.py
@@ -1,7 +1,7 @@
 class Tablero:
     def __init__(self, n):
-        self.n = n
-        self.posiciones = [-1] * n  # Cada índice representa una columna, y el valor la fila
+        self.n = n  # tamaño del tablero (n x n)
+        self.posiciones = [-1] * n  # índice = columna, valor = fila
 
     def es_valido(self, col, fila):
         for c in range(col):
@@ -11,12 +11,12 @@ class Tablero:
                 return False  # misma diagonal
         return True
 
-    def resolver(self, col=0):
-        if col == self.n:
-            return True  # Solución completa
+    def resolver(self, cantidad_reinas, col=0):
+        if col == cantidad_reinas:
+            return True  # colocó todas las reinas necesarias
         for fila in range(self.n):
             if self.es_valido(col, fila):
                 self.posiciones[col] = fila
-                if self.resolver(col + 1):
+                if self.resolver(cantidad_reinas, col + 1):
                     return True
         return False
